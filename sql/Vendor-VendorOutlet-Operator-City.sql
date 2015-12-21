87bd87b95be04b186091bae81a634a97c77be367
Ravi Ailani <rvbugs0@gmail.com>
2015-12-21 12:47:53 +0530

    getVendorsByCity method added

--- sql/Vendor-VendorOutlet-Operator-City.sql
+++ sql/Vendor-VendorOutlet-Operator-City.sql
@@ -372,6 +372,16 @@ DELIMITER ;
 
 DELIMITER $$
 
+DROP PROCEDURE IF EXISTS `loyalty`.`get_vendors_by_city_code`$$
+CREATE PROCEDURE `loyalty`.`get_vendors_by_city_code` (in v_city_code int)
+BEGIN
+select code, username , city_code ,password,password_key,address , mail_id , contact_number,name_of_firm from vendor_view where city_code = v_city_code ;
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
 DROP PROCEDURE IF EXISTS `loyalty`.`vendor_exists_by_mail_id`$$
 CREATE PROCEDURE `loyalty`.`vendor_exists_by_mail_id` (in v_mail_id char(100))
 BEGIN
